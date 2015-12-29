9aef4169e82cdb800acf2de2181c3ba98f02f54e
Alvaro Miranda <kikitux@gmail.com>
2015-12-29 13:49:30 +1300

    formatting

--- myexip.c
+++ myexip.c
@@ -1,6 +1,7 @@
 /*
  * myexip
  * Alvaro Miranda
+ * 20151229
  *
  * C cli to query myexternalip.com
  *
@@ -10,43 +11,38 @@
  * inspired on
  * http://stackoverflow.com/questions/897027/i-want-to-store-a-result-for-character-string-with-libcurl
  * http://marc.info/?l=curl-library&m=100204184522546&w=2
+ * 
  */
 
 #include <stdio.h>
-#include <curl/curl.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <curl/curl.h>
 
 struct ip { char ip[64]; };
 struct ip myip; // global variable that will store ip
 
-size_t  write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
-{
+size_t  write_data(void *ptr, size_t size, size_t nmemb, FILE *stream){
     char buf[size*nmemb+1];
     char *pbuf = &buf[0];
     memset(buf, '\0', size*nmemb+1);
-    size_t i = 0;
-    for(;  i < nmemb ; i++){
-        strncpy(pbuf,ptr,size);
+    for(size_t i = 0 ;  i < nmemb ; i++){
+        strncpy(pbuf, ptr, size);
         pbuf += size;
         ptr += size;
     }
-    
     char *str = "My-External-Ip: "; // the header we are looking for
     size_t lenstr = strlen(str);
-
     if (strncmp(buf, str, lenstr) == 0)
-        strncpy(myip.ip,buf+lenstr, sizeof(myip.ip));
+        strncpy(myip.ip, buf+lenstr, sizeof(myip.ip));
 
     return size * nmemb;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     char *url = "http://ipv4.myexternalip.com"; // default to ipv4
     long http_code = 0;
-
     int c; // getopt counter
 
     opterr = 0;
@@ -54,10 +50,10 @@ int main(int argc, char **argv)
         switch (c)
         {
             case 'h':
-                printf("use: %s [-6]\n",argv[0]);
+                printf("use: %s [-6]\n", argv[0]); // basic usage
                 exit(0);
             case '6':
-                url = "http://ipv6.myexternalip.com";
+                url = "http://ipv6.myexternalip.com"; // override to ipv6
                 break;
             case '?':
                 printf("unknown arg %c\n", optopt);
@@ -68,16 +64,16 @@ int main(int argc, char **argv)
     CURL *curl_handle;
     curl_handle = curl_easy_init();
 
-    curl_easy_setopt(curl_handle,   CURLOPT_URL, url);
-    curl_easy_setopt(curl_handle,   CURLOPT_NOPROGRESS  ,1);
-    curl_easy_setopt(curl_handle,   CURLOPT_HEADER, 1);
-    curl_easy_setopt(curl_handle,   CURLOPT_NOBODY, 1);
-    curl_easy_setopt(curl_handle,   CURLOPT_WRITEFUNCTION,&write_data);
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1);
+    curl_easy_setopt(curl_handle, CURLOPT_HEADER, 1);
+    curl_easy_setopt(curl_handle, CURLOPT_NOBODY, 1);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, &write_data);
     curl_easy_perform(curl_handle);
     curl_easy_getinfo (curl_handle, CURLINFO_RESPONSE_CODE, &http_code);
 
     if (CURLE_OK == 0 && http_code == 200 ){
-        printf("%s",myip.ip);
+        printf("%s", myip.ip);
     } else {
         exit(1);
     }
